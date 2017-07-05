13524d22d5d7caff0833043dfcee3bb6b86438fe
Nik <kslg@23.3>
2017-07-05 17:26:51 +0300

    fix windows

--- app/src/main/java/com/example/nikolay/conecttotestserver/MainActivity.java
+++ app/src/main/java/com/example/nikolay/conecttotestserver/MainActivity.java
@@ -22,6 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.Call;
+import okhttp3.FormBody;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -29,54 +30,43 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-Button button = (Button) findViewById(R.id.button_id);
+        Button button = (Button) findViewById(R.id.button_id);
         button.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("click","click");
+                Log.d("click", "click");
                 new HttpTask().execute();
-
             }
         });
-//
-
-//ObjectMapper mapper = new ObjectMapper();
-//        User user = mapper.readValue(new File("user.json"),User.class);
     }
 
     private class HttpTask extends AsyncTask<Void, Void, Void> {
         private OkHttpClient client = new OkHttpClient();
-//
+        private String result = "unknown";
 
-private String result="unknown";
-//        private char[] test;
-//
-//        public void onClick(View arg0){
-//            EditText test = (EditText) findViewById(R.id.email);
-//            String test1 = test.getText().toString();
-//
-//
-//        }
         @Override
         protected Void doInBackground(Void... voids) {
-
-            String username =((EditText) findViewById(R.id.UserName)).getText().toString();
-            Log.d("username",username);
-
-
+            String username = ((EditText) findViewById(R.id.UserName)).getText().toString();
+            String password1 = ((EditText) findViewById(R.id.password1)).getText().toString();
+            String password2 = ((EditText) findViewById(R.id.password2)).getText().toString();
+            String email = ((EditText) findViewById(R.id.email)).getText().toString();
+            Log.d("username", username);
             HttpUrl.Builder builder = new HttpUrl.Builder()
                     .scheme("http")
-                    .addQueryParameter("username",username)
                     .host("10.10.9.217")
                     .port(8000)
-                    .addPathSegments("/api/register/");
-            final HttpUrl url =builder.build();
-            RequestBody reqbody = RequestBody.create(null, new byte[0]);
+                    .addPathSegments("api/register/");
+            final HttpUrl url = builder.build();
+            RequestBody reqbody = new FormBody.Builder()
+                    .add("username", username)
+                    .add("password1", password1)
+                    .add("password2", password2)
+                    .add("email", email)
+                    .build();
+
             Request request = new Request.Builder()
                     .url(url.toString())
                     .post(reqbody)
@@ -84,7 +74,6 @@ private String result="unknown";
                     .build();
             Call newCall = client.newCall(request);
             Response response;
-
             try {
                 response = newCall.execute();
                 result = response.body().string();
@@ -92,23 +81,13 @@ private String result="unknown";
                 result = e.getMessage();
                 e.printStackTrace();
             }
-//            Document doc = Jsoup.parse(url1);
-//            Elements linkedElements = doc.select("a[href]");
-
-//            for (Element el : linkedElements) {
-//                links.add(el.attr("href"));
-//            }
             return null;
         }
 
         @Override
         protected void onPostExecute(Void op) {
             TextView textView = (TextView) findViewById(R.id.text);
-           textView.setText(result);
-
+            textView.setText(result);
         }
-
-
     }
 }
-//10.10.9.217:8000
